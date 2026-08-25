4c4908420da7be5cc5ab010f6069f9b09de15488
Daniel Abraham Ortiz Chinchilla <2986522690101@ingenieria.usac.edu.gt>
2026-08-25 15:25:33 -0600

    feat: guard de autenticación personalizado

---
+++ Practica_1/api-node/src/common/guards/jwt-auth.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token de autorización ausente.');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      // Adjunta la carga útil al objeto de solicitud para que podamos utilizarla en los controladores.
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Token de autorización inválido o expirado.');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
